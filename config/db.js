6e305f17279e02247ff2062045a9b37c8ef013b6
ahs120 <74141806+ahs120@users.noreply.github.com>
2026-04-22 23:13:21 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
  * Exits the process on failure to ensure the app doesn't run without a DB.
  */
 const connectDB = async () => {
-  if (!process.env.MONGODB_URI) {
+  if (!process.mongodb://hvip11705_db_user:4RnGTo1U2ItgzsL8@ac-d5aysqs-shard-00-00.o4zvqph.mongodb.net:27017,ac-d5aysqs-shard-00-01.o4zvqph.mongodb.net:27017,ac-d5aysqs-shard-00-02.o4zvqph.mongodb.net:27017/?ssl=true&replicaSet=atlas-13096s-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Vero) {
     console.error("❌ FATAL: MONGODB_URI environment variable is not set.");
     process.exit(1);
   }
